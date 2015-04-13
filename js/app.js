b6be5ac06ffd273a3d136f4e1574b9aed5ad4e10
JelenaBarinova <fiodorova.jelena@gmail.com>
2015-04-13 16:15:41 -0700

    New version of blog made from zero

---
+++ js/app.js
@@ -0,0 +1,18 @@
+(function(){
+    
+    var app = angular.module("cookingBlog", ["ngRoute", "cookingBlog.controllers"]);
+    
+    app.config(function($routeProvider){
+        $routeProvider
+            .when("/recipes", {
+                templateUrl: "partials/recipes.html",
+                controller: "RecipesController"
+            })
+            .when("/recipes/:id", {
+                templateUrl: "partials/recipe.html",
+                controller: "RecipeController"
+            })
+            .otherwise({redirectTo:"/recipes"});
+    });
+    
+}());
---
+++ js/script.js
@@ -0,0 +1,44 @@
+(function() {
+
+  var app = angular.module("cookingBlog.controllers", []);
+
+  var RecipesController = function($scope, $http) {
+    
+    var onRecipesComplete = function(response) {
+      $scope.recipes = response.data;
+    };
+
+    var onError = function(reason) {
+      $scope.error = "Could not fetch data";
+    };
+
+    $http.get("http://localhost:8080/api/recipes")
+      .then(onRecipesComplete, onError);
+    
+  };
+  
+  app.controller("RecipesController", ["$scope", "$http", RecipesController]);
+
+  
+  var RecipeController = function($scope, $http, $routeParams) {
+    
+    var id = $routeParams.id;
+    
+    var onRecipeComplete = function(response) {
+      $scope.recipe = response.data;
+      $scope.ingredients = $scope.recipe.Ingredients;
+      $scope.instructions = $scope.recipe.Instructions;
+    };
+
+    var onError = function(reason) {
+      $scope.error = "Could not fetch data";
+    };
+
+    $http.get("http://localhost:8080/api/recipes/" + id)
+      .then(onRecipeComplete, onError);
+    
+  };
+  
+  app.controller("RecipeController", ["$scope", "$http", "$routeParams", RecipeController]);
+
+}());
