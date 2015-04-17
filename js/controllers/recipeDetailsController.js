9469cd5251062dcac7425faf796bdb1f92bde73e
JelenaBarinova <fiodorova.jelena@gmail.com>
2015-04-17 12:10:50 -0700

    Beautified HTML

--- js/controllers/recipeDetailsController.js
+++ js/controllers/recipeDetailsController.js
@@ -1,13 +1,13 @@
 (function () {
-  
+
   'use strict';
 
   var app = angular.module("cookingBlog");
-  
+
   var RecipeController = function (recipeDetails, $scope, $routeParams) {
-    
+
     var id = $routeParams.id;
-    
+
     var onRecipeComplete = function (resource) {
       $scope.recipe = resource;
       $scope.ingredients = $scope.recipe.Ingredients;
@@ -17,12 +17,11 @@
     var onError = function (reason) {
       $scope.error = "Could not fetch data";
     };
-    
-    
+
     recipeDetails.getRecipe(id).$promise.then(onRecipeComplete, onError);
-    
+
   };
-  
+
   app.controller("RecipeController", ["recipeDetails", "$scope", "$routeParams", RecipeController]);
 
 }());
