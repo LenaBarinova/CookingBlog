461172e51f8d93113293a3ed4d4a4bc1afe62c65
JelenaBarinova <fiodorova.jelena@gmail.com>
2015-04-24 15:01:27 -0700

    Click functions moved to a controller for jbNav directive from link function

--- js/directives/jbNav.js
+++ js/directives/jbNav.js
@@ -2,27 +2,33 @@
 
   'use strict';
 
-  var jbNav = function ($location) {
-    return {
-      templateUrl: "././partials/jbNav.html",
-      restrict: "E",
-      link: function (scope, element) {
-        $('#nav > ul').dropotron({
-          mode: 'fade',
-          noOpenerFade: true,
-          hoverDelay: 150,
-          hideDelay: 350
-        });
-        scope.showRecipes = function () {
-          $location.url('/recipes');
-        };
-        scope.showAbout = function () {
-          $location.url('/about');
-        };
-      }
+  var link = function (scope, element) {
+    $('#nav > ul').dropotron({
+      mode: 'fade',
+      noOpenerFade: true,
+      hoverDelay: 150,
+      hideDelay: 350
+    });
+  };
+
+  var jbController = function ($scope, $location) {
+    $scope.showRecipes = function () {
+      $location.url('/recipes');
+    };
+    $scope.showAbout = function () {
+      $location.url('/about');
     };
   };
 
-  app.directive("jbNav", ["$location", jbNav]);
+  var jbNav = function () {
+    return {
+      templateUrl: "././partials/jbNav.html",
+      restrict: "E",
+      link: link,
+      controller: jbController
+    };
+  };
+
+  app.directive("jbNav", jbNav);
 
 }(angular.module("cookingBlog")));
