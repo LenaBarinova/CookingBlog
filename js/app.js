e2c18800790582c27511112dbb56a3e44f83873e
JelenaBarinova <fiodorova.jelena@gmail.com>
2015-05-11 09:58:19 -0700

    Google analytics script changed to support different posts

--- js/app.js
+++ js/app.js
@@ -27,4 +27,18 @@
       });
   });
 
+
+angular.module('yourApp', [])
+  .run(['$rootScope', '$location', '$window', function($rootScope, $location, $window){
+     $rootScope
+        .$on('$stateChangeSuccess',
+            function(event){
+ 
+                if (!$window.ga)
+                    return;
+ 
+                $window.ga('send', 'pageview', { page: $location.path() });
+        });
+}]);
+
 }());
